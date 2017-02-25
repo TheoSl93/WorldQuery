718af7293e3025a7272c2e0da74983061bd4b6e6
Theo Sloot <t.sloot93@gmail.com>
2017-02-25 17:01:42 +0100

    Data type is not any more a Factor in the main dataframes, so theres no need to cast everytime a number or char is needed.
    All work by now, used as savepoint before I keep make changes

--- app.R
+++ app.R
@@ -1,19 +1,41 @@
 #Version in develop
 library(shiny)
 
+#Data import, refactoring the default "factor" type
+
 #Cities:
 cityCSV <- read.csv2("city.csv", header = FALSE, encoding = "UTF-8", sep = ",")
 colnames(cityCSV) <- c("ID", "Name", "CountryCode", "Disctrict", "Population")
 cityCSV$Name <- as.character(cityCSV$Name)
 cityCSV$Disctrict <- as.character(cityCSV$Disctrict)
+cityCSV$CountryCode <- as.character(cityCSV$CountryCode)
 
 #Countries:
 countryCSV <- read.csv2("country.csv", header = FALSE, encoding = "UTF-8", sep = ",")
 colnames(countryCSV) <- c("Code", "Name", "Continent", "Region", "SurfaceArea", "IndepYear", "Population", "LifeExpentancy", "GNP", "GNPOld", "LocalName", "GovernmentForm", "HeadOfState", "Capital", "Code2")
+countryCSV$Code <- as.character(countryCSV$Code)
+countryCSV$Name <- as.character(countryCSV$Name)
+countryCSV$Continent <- as.character(countryCSV$Continent)
+countryCSV$Region <- as.character(countryCSV$Region)
+countryCSV$SurfaceArea <- as.numeric(as.character(countryCSV$SurfaceArea))
+countryCSV$IndepYear <- as.numeric(as.character(countryCSV$IndepYear))
+countryCSV$Population <- as.numeric(as.character(countryCSV$Population))
+countryCSV$LifeExpentancy <- as.numeric(as.character(countryCSV$LifeExpentancy))
+countryCSV$GNP <- as.numeric(as.character(countryCSV$GNP))
+countryCSV$GNPOld <- as.numeric(as.character(countryCSV$GNPOld))
+countryCSV$LocalName <- as.character(countryCSV$LocalName)
+countryCSV$GovernmentForm <- as.character(countryCSV$GovernmentForm)
+countryCSV$HeadOfState <- as.character(countryCSV$HeadOfState)
+countryCSV$Capital <- as.numeric(as.character(countryCSV$Capital))
+countryCSV$Code2<- as.character(countryCSV$Code2)
 
 #Languages:
 idiomaCSV <- read.csv2("countrylanguage.csv", header = FALSE, encoding = "UTF-8",sep = ",")
 colnames(idiomaCSV) <- c("CountryCode", "Language", "IsOfficial", "Percentage")
+idiomaCSV$CountryCode <-as.character(idiomaCSV$CountryCode)
+idiomaCSV$Language <- as.character(idiomaCSV$Language)
+idiomaCSV$IsOfficial <- as.logical(idiomaCSV$IsOfficial)
+idiomaCSV$Percentage <- as.numeric(as.character(idiomaCSV$Percentage))
 
 # Define UI:
 ui <-  pageWithSidebar(
@@ -31,14 +53,14 @@ ui <-  pageWithSidebar(
   mainPanel(
     tabsetPanel(
       tabPanel("List:",
-               textOutput("relatedDataL"),
+               htmlOutput("relatedDataL"),
+               br(),
                dataTableOutput("listResult")),
       tabPanel("Charts:",
-               textOutput("relatedDataP"),
+               htmlOutput("relatedDataP"),
+               br(),
                plotOutput("plotResult"))
     )
-    
-    
   )
 )
 
@@ -56,34 +78,26 @@ server <- function(input, output) {
     if(chosenList == "Cities by countries"){
       
       data <- countryCSV$Name
-      
       selectInput("despSecondChoice", "Cuontries", choices = sort(data))
       
     }else if(chosenList == "Countries by regions"){
       
       data <- unique(countryCSV$Region)
-      
       selectInput("despSecondChoice", "Regions", choices = sort(data))
       
     }else if(chosenList == "Countries by language"){
       
-      idiomas <- as.vector(unique(idiomaCSV$Language))
+      idiomas <- unique(idiomaCSV$Language)
       data <-c()
       for(idioma in idiomas){
         
         if(length(subset(idiomaCSV, Language == idioma)[,1])>=2){
-          
           data <-c(data, idioma)
-          
         }
-        
       }
       
       selectInput("despSecondChoice", "Languages", choices = sort(data))
-      
     }
-    
-    
   })
   
   #Population slider: DONE
@@ -93,12 +107,9 @@ server <- function(input, output) {
     chosenList <- input$despFirstChoiceL
     
     if(chosenList == "Cities by countries"){
-      
-      countryCSV$Code <- as.character(countryCSV$Code)
+
       datosPais <- subset(countryCSV, Name == sec)
-      
       codigoPais <- as.character(datosPais[1])
-      
       ciudades <- subset(cityCSV, CountryCode == codigoPais)
       
       population <- ciudades$Population
@@ -106,13 +117,12 @@ server <- function(input, output) {
     }else if(chosenList == "Countries by regions"){
       
       paisesXregion <- subset(countryCSV, Region == sec)
-      
       population <- paisesXregion$Population
       
     }else if(chosenList == "Countries by language"){
       
       
-      codPaisesXidioma <- as.vector(subset(idiomaCSV, Language == sec))
+      codPaisesXidioma <- subset(idiomaCSV, Language == sec)
       codPaisesXidioma <- as.vector(codPaisesXidioma[,1])
       
       paisesXidioma <- subset(countryCSV, Code %in% codPaisesXidioma)
@@ -123,12 +133,10 @@ server <- function(input, output) {
     minimum <- min(population)
     
     sliderInput("slidAnio", label = "Population", min = minimum, max = maximum, value = c(minimum,maximum))
-    
   })
   
   #Suerface slider: DONE
   output$slidSurfaceL <- renderUI({
-    
     
     sec <- input$despSecondChoice
     chosenList <- input$despFirstChoiceL
@@ -136,20 +144,16 @@ server <- function(input, output) {
     if(chosenList == "Countries by regions"){
       
       paisesXregion <- subset(countryCSV, Region == sec)
-      
-      surface <- as.integer(as.vector(paisesXregion$SurfaceArea))
-      
+      surface <-paisesXregion$SurfaceArea
       
     }else if(chosenList == "Countries by language"){
       
-      codPaisesXidioma <- as.vector(subset(idiomaCSV, Language == sec))
+      codPaisesXidioma <- subset(idiomaCSV, Language == sec)
       codPaisesXidioma <- as.vector(codPaisesXidioma[,1])
       
       paisesXidioma <- subset(countryCSV, Code %in% codPaisesXidioma)
       
-      surface <- as.integer(as.vector(paisesXidioma$SurfaceArea))
-      
-      
+      surface <- paisesXidioma$SurfaceArea
     }
     
     if(chosenList != "Cities by countries"){
@@ -165,28 +169,24 @@ server <- function(input, output) {
   
   #GNP filter: DONE
   output$slidGNPL <- renderUI({
-    
-    
+  
     sec <- input$despSecondChoice
     chosenList <- input$despFirstChoiceL
     
     if(chosenList == "Countries by regions"){
       
       paisesXregion <- subset(countryCSV, Region == sec)
-      
-      GNP <- as.integer(as.vector(paisesXregion$GNP))
+      GNP <- paisesXregion$GNP
       
       
     }else if(chosenList == "Countries by language"){
       
-      codPaisesXidioma <- as.vector(subset(idiomaCSV, Language == sec))
+      codPaisesXidioma <- subset(idiomaCSV, Language == sec)
       codPaisesXidioma <- as.vector(codPaisesXidioma[,1])
       
       paisesXidioma <- subset(countryCSV, Code %in% codPaisesXidioma)
       
-      GNP <- as.integer(as.vector(paisesXidioma$GNP))
-      
-      
+      GNP <- paisesXidioma$GNP
     }
     
     if(chosenList != "Cities by countries"){
@@ -195,7 +195,6 @@ server <- function(input, output) {
       minimum <- min(GNP)
       
       sliderInput("slipGNP", label = "GNP", min = minimum, max = maximum, value = c(minimum,maximum))
-      
     }
     
   })
@@ -205,12 +204,122 @@ server <- function(input, output) {
   
   #Result: Country's info, only with "cities by country"
   output$relatedDataL <- renderText({
-    paste("datos del pais:" , input$despSecondChoice)
+    
+    chosenList <- input$despFirstChoiceL
+    secChoice <- input$despSecondChoice
+    
+    if(chosenList == "Cities by countries"){
+      
+      chosenCountry <- subset(countryCSV, Name == secChoice)
+      codCapital <- chosenCountry$Capital
+      capital <- subset(cityCSV, ID == codCapital)
+      
+    
+      HTML(paste("<font size=3>","<b>","About", secChoice,"</b>","</font>", ": ",
+                 "<b>","Continent: ","</b>", chosenCountry$Continent,"|",
+                 "<b>", "Region: ", "</b>", chosenCountry$Region,"|",
+                 "<b>", "Country code: ","</b>"  ,chosenCountry$Code,"|",
+                 "<b>", "Capital: ", "</b>", capital$Name,"|",
+                 "<b>", "Surface: ", "</b>", chosenCountry$Surface,"|",
+                 "<b>", "Population: ", "</b>", chosenCountry$Population,"|",
+                 "<b>", "Life expentancy: ", "</b>", chosenCountry$LifeExpentancy
+                  )
+           )
+      
+    }else if(chosenList == "Countries by regions"){
+      
+      #Countries per region:
+      chosenRegion <- subset(countryCSV, Region == secChoice)
+      
+      continent <- unique(chosenRegion$Continent)
+      population <- sum(chosenRegion$Population)
+      surface <- sum(chosenRegion$SurfaceArea)
+      averLifeExpentancy <- mean(chosenRegion$LifeExpentancy,na.rm = TRUE)
+      averLifeExpentancy <- round(averLifeExpentancy, digits = 2)
+      
+      
+      HTML(paste("<font size=3>","<b>","About", secChoice,"</b>","</font>", ": ",
+                 "<b>","Continent: ","</b>", continent,"|",
+                 "<b>","Population: ","</b>", population,"|",
+                 "<b>","Surface: ","</b>", surface,"|",
+                 "<b>","Average life expentancy: ","</b>", averLifeExpentancy
+      ))
+
+      
+      ####REVISAR
+    }else if(chosenList == "Countries by language"){
+      
+      #Countries per language:
+      CountriesLangOff <- subset(idiomaCSV, Language == secChoice & IsOfficial == TRUE)
+      
+      #List of country codes, where its official
+      CountriesLangOff <- as.vector(CountriesLangOff$CountryCode)
+      
+      #List of countrys, from the codes
+      CountriesLangOff <- subset(countryCSV, Code %in% CountriesLangOff)
+      
+      #List of names of the countries where it's official
+      CountriesLangOff <- as.vector(CountriesLangOff$Name)
+      #####################-------------------------------------------------------------------
+      
+      
+      #Pendiente de arreglar porque se puede hacer más corto, ver script Lineaw 70
+      
+      # language =input$despSecondChoice
+      # 
+      # countriesByLanguage <- as.vector(subset(idiomaCSV, Language == language))
+      # codesCBL <- as.vector(countriesByLanguage[,1])
+      # 
+      # finalList <- subset(countryCSV, Code %in% codesCBL)
+      # 
+      # finalList <- finalList[,-c(6,8,10,12,13,15)]
+      # capitalsCodes <- as.integer(as.vector(finalList$Capital))
+      # capitalNames <- subset(cityCSV, ID %in% capitalsCodes)
+      # 
+      # # #Order by code before bind all:
+      # finalList <- finalList[order(finalList$Code),]
+      # capitalNames <- capitalNames[order(capitalNames$CountryCode),]
+      # countriesByLanguage <- countriesByLanguage[order(countriesByLanguage$CountryCode),]
+      # 
+      # finalList <- cbind.data.frame(finalList, capitalNames$Name,  countriesByLanguage$Percentage)
+      # 
+      # finalList$Capital <- NULL
+      # finalList$Code <- NULL
+      # 
+      # names(finalList)[8] <- "Capital"
+      # names(finalList)[9] <- "Percentage"
+      
+      
+      ##############---------------------------------------------------------------------------------------
+      
+      
+      
+      
+      # if(length(CountriesLangOff == 0)){
+      #   HTML(paste("<font size=3>","<b>","About", secChoice,"</b>","</font>", ": ",
+      #               "not an official language: ", "|"
+      #   )) 
+      # }else{
+      #    HTML(paste("<font size=3>","<b>","About", secChoice,"</b>","</font>", ": ",
+      #            "<b>", "Official in: ", "</b>", paste(CountriesLangOff, sep ="", collapse = ", "), "|"
+      #            ))
+      #   
+      # }
+      
+      HTML(paste("<font size=3>","<b>","About", secChoice,"</b>","</font>", ": ",
+                            "<b>", "Official in: ", "</b>", paste(CountriesLangOff, sep ="", collapse = ", "), "|"
+                            ))
+     
+      
+    }
+ 
   })
   
   #Result: Country's info, only with "cities by country"
-  output$relatedDataP <- renderText({
+    output$relatedDataP <- renderText({
+    
     paste("datos del pais:" , input$despSecondChoice)
+    
   })
   
   #Result (List): the result, whit the filters, as a list
@@ -247,7 +356,7 @@ server <- function(input, output) {
       
       finalList <- finalList[order(finalList$Code),]
       capitalName <- capitalName[order(capitalName$CountryCode),]
-      finalList <- cbind(finalList, capitalName$Name)
+      finalList <- cbind.data.frame(finalList, capitalName$Name)
       finalList$Capital <- NULL
       finalList$Code <- NULL
       names(finalList)[9] <- "Capital"
@@ -271,17 +380,13 @@ server <- function(input, output) {
       capitalNames <- capitalNames[order(capitalNames$CountryCode),]
       countriesByLanguage <- countriesByLanguage[order(countriesByLanguage$CountryCode),]
 
-      finalList <- cbind(finalList, capitalNames$Name,  countriesByLanguage$Percentage)
+      finalList <- cbind.data.frame(finalList, capitalNames$Name,  countriesByLanguage$Percentage)
       
       finalList$Capital <- NULL
       finalList$Code <- NULL
       
       names(finalList)[8] <- "Capital"
       names(finalList)[9] <- "Percentage"
-      
-      
-      
-      
     }
     
     #Population filter:
@@ -323,20 +428,14 @@ server <- function(input, output) {
     }else if(chosenList == "Countries by regions"){
       
       region <- input$despSecondChoice
-      
       xlabel <- "Countries"
-      
       finalList <- as.vector(subset(countryCSV, Region == region))
-      
-      
+
     }else if(chosenList == "Countries by language"){
       
       idioma =input$despSecondChoice
-      
       codPaisesXidioma <- as.vector(subset(idiomaCSV, Language == idioma))
       codPaisesXidioma <- as.vector(codPaisesXidioma[,1])
-      
-      
       xlabel <- "Countries"
       finalList <- subset(countryCSV, Code %in% codPaisesXidioma)
       
@@ -390,9 +489,7 @@ server <- function(input, output) {
     box()
     
   })
-  
-  
-  
+
 }
 
 # Run the application 
